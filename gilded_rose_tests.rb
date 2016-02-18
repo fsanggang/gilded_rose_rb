5e6570e5c0e7da995ce582d68a8e7efd8f404e4c
Fiona Sanggang <fsanggang@gmail.com>
2016-02-18 15:34:14 +1300

    Add tests for 'Backstage passes to a TAFKAL80ETC concert'

--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -3,6 +3,64 @@ require "minitest/autorun"
 
 class GildedRoseTest < Minitest::Test
 
+  def test_backstage_sell_in_minus_one
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, -2
+    assert_equal items[0].quality, 0
+  end
+
+  def test_backstage_sell_in_five
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 4
+    assert_equal items[0].quality, 3
+  end
+
+  def test_backstage_sell_in_ten
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 9
+    assert_equal items[0].quality, 2
+  end
+
+  def test_backstage_sell_in_eleven
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 11, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 10
+    assert_equal items[0].quality, 1
+  end
+
+  def test_backstage_quality_zero
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 0
+  end
+
+  def test_backstage_quality_forty_nine
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 49)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 0
+  end
+
+  def test_backstage_quality_fifty
+    items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 0
+  end
+
+
+
   def test_brie_sell_in_minus_one
     items = [Item.new("Aged Brie", -1, 0)]
     GildedRose.new(items).update_quality()
