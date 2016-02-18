77e23f1bab27a53a49e414f733ea44e7728fb14d
Fiona Sanggang <fsanggang@gmail.com>
2016-02-18 15:12:13 +1300

    Added tests for 'Sulfuras, Hand of Ragnaros'

--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -3,10 +3,60 @@ require "minitest/autorun"
 
 class GildedRoseTest < Minitest::Test
 
-  def test_foo
-    items = [Item.new("foo", 0, 0)]
+  def test_sulfuras_sell_in_minus_one
+    items = [Item.new("Sulfuras, Hand of Ragnaros", -1, 0)]
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "foo"
+
+    assert_equal items[0].sell_in, -1
+    assert_equal items[0].quality, 0
+  end
+
+  def test_sulfuras_sell_in_five
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 5
+    assert_equal items[0].quality, 0
+  end
+
+  def test_sulfuras_sell_in_ten
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 10
+    assert_equal items[0].quality, 0
+  end
+
+  def test_sulfuras_sell_in_eleven
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 11, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 11
+    assert_equal items[0].quality, 0
+  end
+
+  def test_sulfuras_quality_zero
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 0)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 0
+    assert_equal items[0].quality, 0
+  end
+
+  def test_sulfuras_quality_forty_nine
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 49)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 0
+    assert_equal items[0].quality, 49
+  end
+
+  def test_sulfuras_quality_fifty
+    items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
+    GildedRose.new(items).update_quality()
+
+    assert_equal items[0].sell_in, 0
+    assert_equal items[0].quality, 50
   end
 
 end
