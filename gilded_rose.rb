1dfd65d98892762687992c3c3b79f560bfa31919
Fiona Sanggang <fsanggang@gmail.com>
2016-02-18 16:55:06 +1300

    Add GildedRose#update_quality_backstage, all tests passing.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -7,13 +7,15 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       case item.name
-      when "Sulfuras, Hand of Ragnaros"
-        update_quality_sulfuras(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        update_quality_backstage(item)
       when "Aged Brie"
         update_quality_brie(item)
+      when "Sulfuras, Hand of Ragnaros"
+        update_quality_sulfuras(item)
       else
+        puts "WAT"
       end
-
 
       # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       #   if item.quality > 0
@@ -61,7 +63,13 @@ class GildedRose
     end
   end
 
-  def update_quality_sulfuras(item)
+  def update_quality_backstage(item)
+    item.sell_in = item.sell_in - 1
+
+    item.quality = item.quality + 3 if item.sell_in > 0 && item.sell_in < 6
+    item.quality = item.quality + 2 if item.sell_in >= 6 && item.sell_in < 10
+    item.quality = item.quality + 1 if item.sell_in >= 10
+    item.quality = 0 if item.quality >= 49
   end
 
   def update_quality_brie(item)
@@ -69,6 +77,9 @@ class GildedRose
 
     item.quality = item.quality + 1 if item.sell_in <= 0 unless item.quality >= 50
     item.quality = item.quality + 1 unless item.quality >= 50
+  end
+
+  def update_quality_sulfuras(item)
   end
 end
 
